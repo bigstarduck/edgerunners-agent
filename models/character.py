32de0b608847ca662590e4de7987abac16f389fc
Jordan <bigstarduck@gmail.com>
2024-09-12 22:15:19 -0600

    Started adding calls for sql data

--- models/character.py
+++ models/character.py
@@ -1,95 +1,100 @@
-character_data = {
-    "name": "Neon Dream",
-    
-    "stats": [
-        {
-            "name": "INT",
-            "value": 7, 
-        },
-        { 
-            "name": "REF", 
-            "value": 7, 
-        },
-        { 
-            "name": "DEX", 
-            "value":  6, 
-        },
-        { 
-            "name": "TECH",
-            "value": 5,
-        },
-        { 
-            "name": "COOL",
-            "value": 7,
-        },
-        { 
-            "name": "WILL",
-            "value": 6, 
-        },
-        { 
-            "name": "LUCK", 
-            "value": 6, 
-        },
-        { 
-            "name": "MOVE", 
-            "value": 7, 
-        },
-        { 
-            "name": "BODY", 
-            "value": 7, 
-        },
-        { 
-            "name": "EMP",  
-            "value": 5, 
-        },
-    ],
-
-    "health": {
-        "hit_points": [
-            { 
-                "name": "HP",
-                "value": 53,
-            }
-        ],
-        "armor": [
-            { 
-                "name": "Head",
-                "value" : 9, 
-            },
-            { 
-                "name": "Body",
-                "value": 10, 
-            },
-        ],
-    },
-
-    "skills": [
-        {   
-            "name": "Concentration",
-            "stat": "WILL",
-            "value": 10,
-        },
-        {
-            "name": "Perception",
-            "stat": "INT",
-            "value": 12,
-        },
-        {
-            "name": "Handgun",
-            "stat": "REF",
-            "value": 14,
-        },
-        {
-            "name": "Basic Tech",
-            "stat": "TECH",
-            "value": 9,
-        },
-        {
-            "name": "Hacking",
-            "stat": "TECH",
-            "value": 15,
-        },
+from db import app, Stat, Armor, Health
+with app.app_context():
+    character_stats = Stat.query.all()
+    character_health = Health.query.all()
+    character_armor = Armor.query.all()
+    character_data = {"stats": Stat.query.all(), "health": Health.query.all(), "armor": Armor.query.all()}
+    #     "name": "Neon Dream",
         
-    ]
+    #     "stats": [
+    #         {
+    #             "name": "INT",
+    #             "value": 7, 
+    #         },
+    #         { 
+    #             "name": "REF", 
+    #             "value": 7, 
+    #         },
+    #         { 
+    #             "name": "DEX", 
+    #             "value":  6, 
+    #         },
+    #         { 
+    #             "name": "TECH",
+    #             "value": 5,
+    #         },
+    #         { 
+    #             "name": "COOL",
+    #             "value": 7,
+    #         },
+    #         { 
+    #             "name": "WILL",
+    #             "value": 6, 
+    #         },
+    #         { 
+    #             "name": "LUCK", 
+    #             "value": 6, 
+    #         },
+    #         { 
+    #             "name": "MOVE", 
+    #             "value": 7, 
+    #         },
+    #         { 
+    #             "name": "BODY", 
+    #             "value": 7, 
+    #         },
+    #         { 
+    #             "name": "EMP",  
+    #             "value": 5, 
+    #         },
+    #     ],
 
-}
+    #     "health": {
+    #         "hit_points": [
+    #             { 
+    #                 "name": "HP",
+    #                 "value": 53,
+    #             }
+    #         ],
+    #         "armor": [
+    #             { 
+    #                 "name": "Head",
+    #                 "value" : 9, 
+    #             },
+    #             { 
+    #                 "name": "Body",
+    #                 "value": 10, 
+    #             },
+    #         ],
+    #     },
+
+    #     "skills": [
+    #         {   
+    #             "name": "Concentration",
+    #             "stat": "WILL",
+    #             "value": 10,
+    #         },
+    #         {
+    #             "name": "Perception",
+    #             "stat": "INT",
+    #             "value": 12,
+    #         },
+    #         {
+    #             "name": "Handgun",
+    #             "stat": "REF",
+    #             "value": 14,
+    #         },
+    #         {
+    #             "name": "Basic Tech",
+    #             "stat": "TECH",
+    #             "value": 9,
+    #         },
+    #         {
+    #             "name": "Hacking",
+    #             "stat": "TECH",
+    #             "value": 15,
+    #         },
+            
+    #     ]
+
+    # }
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, send_from_directory
-from models import character
+
 
 # creates a Flask application 
 app = Flask(__name__) 
-  
+
+from models import character
+
 # @app.route("/") 
 # def hello(): 
 #     return "Hello World"
@@ -12,13 +14,29 @@ app = Flask(__name__)
 def index():
     return "<a href=index.html>Click Me Now</a>"
 
+#Serves the static pages
 @app.route('/<path:path>')
 def serve_static(path):
     return send_from_directory('static', path)
 
 @app.route('/get_character_data')
-def send_health_data():
+def send_character_data():
+    print(character.character_data)
+    for armor in character.character_data["armor"] :
+        print(armor)
     return character.character_data
+
+@app.route('/get_character_stats')
+def send_character_stats():
+    return character.character_stats
+
+@app.route('/get_character_health')
+def send_character_health():
+    return character.character_health
+
+@app.route('/get_character_armor')
+def send_character_armor():
+    return character.character_armor
 
 # run the application 
 if __name__ == "__main__": 
